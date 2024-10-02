33f6484b75b61221eb515a1bbc4476a5023adf6e
Shreyash Gharod <shreyashgharod@gmail.com>
2024-10-02 23:19:36 +0530

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://gallerypng.com/wp-content/uploads/2024/07/troll-face-png-hd-image-750x750.png"
+        className="header--image"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
+};
